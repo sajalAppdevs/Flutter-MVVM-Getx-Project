987c8fec0629a98e0d2229331bf197758501d47c
Hamad Anwar <rh676838@gmail.com>
2023-08-04 10:56:49 +0500

    localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:mvvm_getx/res/getx_localization/getx_localization.dart';
 import 'package:mvvm_getx/res/routes/routes.dart';
 
 void main() {
@@ -9,10 +10,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      translations: Languages(),
+      locale: Locale('en_US'),
+      fallbackLocale: Locale('en_US'),
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
---
+++ lib/res/getx_localization/getx_localization.dart
@@ -0,0 +1,18 @@
+import 'package:get/get_navigation/src/root/internacionalization.dart';
+
+class Languages extends Translations{
+
+  @override
+  Map<String, Map<String,String>> get keys =>{
+
+    'en_US' : {
+      'email_hint' : "Email Hint"
+    },
+
+
+    'ur_PK' : {
+      'email_hint' : "Write in urdu keyboard"
+    }
+
+  };
+}
